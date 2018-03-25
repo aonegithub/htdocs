0f13f98ecfc9630151262405b65a87942df57415
A-One <aone0960@gmail.com>
2018-03-25 17:24:15 +0800

    OK

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-777888999000                    <a href="https://laravel.com/docs">Documentation</a>
+777888999000111                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
