e90f42313fbd484e4fb2ed95ad9546545ef42f90
aonegithub <aone0960@gmail.com>
2018-03-29 21:56:37 +0800

    安裝Image套件

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@main');
 Route::get('/dt', function () {
     return date("Y-m-d H:i:s");
 });
