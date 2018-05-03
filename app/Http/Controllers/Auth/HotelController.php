c95c4831089dc67aa928f63041b93578bfa4adbe
aone_long <aone0960@gmail.com>
2018-05-03 09:58:22 +0800

    yyyy

--- app/Http/Controllers/Auth/HotelController.php
+++ app/Http/Controllers/Auth/HotelController.php
@@ -33,7 +33,7 @@ class HotelController extends Controller
     }
 // 飯店管理新增介面View
     public function add(){
-        $auth_key ='1'; //新增管理員權限碼1
+        $auth_key ='1'; //新增管理員權限碼1rrr
         //var_dump($auth_array);
         $auth_array =explode(',', session()->get('manager_auth'));
         if(!in_array($auth_key,$auth_array)){
